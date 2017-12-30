2608c37882b4269de6e20daa0e63455ef43264d5
Evan <evanbyrd16@gmail.com>
2017-12-30 15:02:09 -0800

    Implemented the means of scanning the dictionary and getting the user's input word.

--- anagram.go
+++ anagram.go
@@ -1,21 +1,59 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
-	args := os.Args
-	argstest := os.Args[1:]
-	if len(args) < 1 {
-		fmt.Println("You need to specify the dictionary file.")
-	} else {
-		fmt.Println(argstest)
-		getDictionary()
+	// Get the dictionary file.
+	dictionary, dictError := getDictionary("./dictionary.txt")
+	if dictError != nil {
+		panic(dictError)
 	}
+
+	// Prompt the user to input a word.
+	word, wordError := getUserInput()
+	if wordError != nil {
+		panic(dictError)
+	}
+
+	// Collect anagrams out of dictionary
+	anagrams, anagError := getAnagrams(dictionary, word)
+	if anagError != nil {
+		panic(anagError)
+	}
+
+	fmt.Println(anagrams)
 }
 
-func setDictionary() {
-	fmt.Println("this will work eventually")
+func getDictionary(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func getUserInput() (string, error) {
+	textReader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter a word: ")
+	word, err := textReader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+
+	return word, err
+}
+
+func getAnagrams(dictionary []string, word string) ([]string, error) {
+	return nil, nil
 }
