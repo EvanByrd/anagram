6340d866de3de301b4157a2612ed1b72f2f99e19
Evan <evanbyrd16@gmail.com>
2017-12-29 19:59:03 -0800

    More tests.

--- anagram.go
+++ anagram.go
@@ -7,14 +7,15 @@ import (
 
 func main() {
 	args := os.Args
+	argstest := os.Args[1:]
 	if len(args) < 1 {
 		fmt.Println("You need to specify the dictionary file.")
 	} else {
-		setAnagram()
+		fmt.Println(argstest)
+		getDictionary()
 	}
 }
 
-func setAnagram() {
+func setDictionary() {
 	fmt.Println("this will work eventually")
-	// test
 }
