c7370afa9f1f82d85714d1b507c12735e6dd97d2
Evan <evanbyrd16@gmail.com>
2017-12-29 19:31:02 -0800

    First commit. Set up the main function and some function to do something with.

---
+++ anagram.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args
+	if len(args) < 1 {
+		fmt.Println("You need to specify the dictionary file.")
+	} else {
+		setAnagram()
+	}
+}
+
+func setAnagram() {
+	fmt.Println("this will work eventually")
+}
