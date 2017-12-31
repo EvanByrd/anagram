a0c0eb7dd0abe408e44c83df13606b067db6b55a
Evan <evanbyrd16@gmail.com>
2017-12-30 23:05:34 -0800

    Set up the necessary logic for collecting the anagrams.

--- anagram.go
+++ anagram.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strings"
 )
 
 func main() {
@@ -13,21 +15,33 @@ func main() {
 		panic(dictError)
 	}
 
+	var word string
+	var wordError error
 	// Prompt the user to input a word.
-	word, wordError := getUserInput()
+	word, wordError = getUserInput()
 	if wordError != nil {
 		panic(dictError)
 	}
 
-	// Collect anagrams out of dictionary
-	anagrams, anagError := getAnagrams(dictionary, word)
-	if anagError != nil {
-		panic(anagError)
-	}
+	for !strings.EqualFold(word, "exit\n") {
+		// Collect anagrams out of dictionary
+		anagrams, anagError := getAnagrams(dictionary, word)
+		if anagError != nil {
+			panic(anagError)
+		}
 
-	fmt.Println(anagrams)
+		fmt.Println(anagrams)
+
+		// Prompt the user to input another word.
+		word, wordError = getUserInput()
+		if wordError != nil {
+			panic(dictError)
+		}
+
+	}
 }
 
+// Go fetch the dictionary from the specified path
 func getDictionary(path string) ([]string, error) {
 	file, err := os.Open(path)
 	if err != nil {
@@ -43,6 +57,7 @@ func getDictionary(path string) ([]string, error) {
 	return lines, scanner.Err()
 }
 
+// Display a prompt for user input and then return the input.
 func getUserInput() (string, error) {
 	textReader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter a word: ")
@@ -54,6 +69,20 @@ func getUserInput() (string, error) {
 	return word, err
 }
 
+// Gather all of the anagrams of the given word.
 func getAnagrams(dictionary []string, word string) ([]string, error) {
-	return nil, nil
+	var output []string
+	wordLetters := SortString(word)
+	for i := 0; i < len(dictionary); i++ {
+		if strings.EqualFold(wordLetters, SortString(dictionary[i]+"\n")) && !strings.EqualFold(word, dictionary[i]+"\n") {
+			output = append(output, dictionary[i])
+		}
+	}
+	return output, nil
+}
+
+func SortString(w string) string {
+	s := strings.Split(w, "")
+	sort.Strings(s)
+	return strings.Join(s, "")
 }
