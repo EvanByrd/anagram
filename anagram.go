7190e0b49082ab029054ad2c8299257892847dd3
Evan <evanbyrd16@gmail.com>
2017-12-31 09:02:50 -0800

    Finished version one of the challenge.

--- anagram.go
+++ anagram.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"sort"
 	"strings"
+	"time"
 )
 
 func main() {
+	var dictionaryPath = flag.String("dict", "./dictionary.txt", "")
+	dictStartTime := time.Now()
+
 	// Get the dictionary file.
-	dictionary, dictError := getDictionary("./dictionary.txt")
+	dictionary, dictError := getDictionary(*dictionaryPath)
 	if dictError != nil {
 		panic(dictError)
 	}
+	dictEndTime := time.Now()
+	fmt.Println(dictEndTime.Sub(dictStartTime))
 
 	var word string
 	var wordError error
@@ -23,14 +30,21 @@ func main() {
 		panic(dictError)
 	}
 
-	for !strings.EqualFold(word, "exit\n") {
+	for !strings.EqualFold(word, "exit") {
+		anagramStartTime := time.Now()
 		// Collect anagrams out of dictionary
 		anagrams, anagError := getAnagrams(dictionary, word)
 		if anagError != nil {
 			panic(anagError)
 		}
+		anagramEndTime := time.Now()
 
-		fmt.Println(anagrams)
+		if len(anagrams) == 0 {
+			fmt.Println("No anagrams found for " + word)
+		} else {
+			fmt.Println(anagrams)
+		}
+		fmt.Println(anagramEndTime.Sub(anagramStartTime))
 
 		// Prompt the user to input another word.
 		word, wordError = getUserInput()
@@ -60,13 +74,15 @@ func getDictionary(path string) ([]string, error) {
 // Display a prompt for user input and then return the input.
 func getUserInput() (string, error) {
 	textReader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter a word: ")
+	fmt.Print("\nEnter a word: ")
 	word, err := textReader.ReadString('\n')
 	if err != nil {
 		return "", err
 	}
 
-	return word, err
+	output := strings.Replace(word, "\n", "", -1)
+
+	return output, err
 }
 
 // Gather all of the anagrams of the given word.
@@ -74,10 +90,11 @@ func getAnagrams(dictionary []string, word string) ([]string, error) {
 	var output []string
 	wordLetters := SortString(word)
 	for i := 0; i < len(dictionary); i++ {
-		if strings.EqualFold(wordLetters, SortString(dictionary[i]+"\n")) && !strings.EqualFold(word, dictionary[i]+"\n") {
+		if strings.EqualFold(wordLetters, SortString(dictionary[i])) && !strings.EqualFold(word, dictionary[i]) {
 			output = append(output, dictionary[i])
 		}
 	}
+
 	return output, nil
 }
 
